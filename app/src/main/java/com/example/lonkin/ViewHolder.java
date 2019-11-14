71b6098c198022a4181c51251a146ed9e36283bc
x4ker <pasha230899@gmail.com>
2019-11-14 16:33:36 +0300

    F

---
+++ app/src/main/java/com/example/lonkin/ViewHolder.java
@@ -0,0 +1,17 @@
+package com.example.lonkin;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView message;
+
+    public ViewHolder(View itemView){
+        super(itemView);
+        message = itemView.findViewById(R.id.message_item);
+    }
+
+}
